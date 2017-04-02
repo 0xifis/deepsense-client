c12a6ef3fa08d8099dfd051d490ebdc76b4c7737
Aleksandr Vorontsov <vorontsov98@gmail.com>
2017-04-02 13:19:24 +0100

    Attempt at more graph fixing

--- chartjs.js
+++ chartjs.js
@@ -14,9 +14,33 @@ var data = {
     }, ]
 };
 
+var data2 = {
+    labels: ["Happy", "Relaxed", "Sad", "Tense"],
+    datasets: [{
+        backgroundColor: "rgba(179,181,198,0.2)",
+        borderColor: "rgba(179,181,198,1)",
+        data: [],
+    },
+    {
+        backgroundColor: "rgba(179,181,198,0.2)",
+        borderColor: "rgba(179,181,198,1)",
+        data: [],
+    },
+    {
+        backgroundColor: "rgba(179,181,198,0.2)",
+        borderColor: "rgba(179,181,198,1)",
+        data: [],
+    },
+    {
+        backgroundColor: "rgba(179,181,198,0.2)",
+        borderColor: "rgba(179,181,198,1)",
+        data: [],
+    }]
+};
+
 var linearGraph = new Chart(cty, {
     type: "line",
-    data: data,
+    data: data2,
     options: {
         backgroundColor: '#fff',
         animation: {
@@ -45,6 +69,12 @@ var radarChart = new Chart(ctx, {
         },
         maintainAspectRatio: true,
         responsive: true,
+        scale: {
+            display: true,
+            ticks: {
+                beginAtZero: true,
+            }
+        },
     }
 });
 
@@ -55,7 +85,11 @@ $(function() {
                 cache: false
             })
             .done(function(raw) {
-                linearGraph.data.datasets[0].data = [raw.alpha, raw.beta, raw.gamma, raw.theta];
+                linearGraph.data.datasets[0].data.push(raw.alpha);
+                linearGraph.data.datasets[1].data.push(raw.beta);
+                linearGraph.data.datasets[2].data.push(raw.gamma);
+                linearGraph.data.datasets[3].data.push(raw.theta);
+                console.log(  linearGraph.data.datasets[0])
                 linearGraph.update();
             });
     }, 1000)
